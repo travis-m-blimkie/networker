b732911d4f1e33dcbd903f4516168dd59fcb5752
Travis <travis.m.blimkie@gmail.com>
2022-02-25 15:58:23 -0800

    Updated built-in data object to duplicate interactions, UBC, and other promiscuous proteins.

--- data-raw/innatedb.R
+++ data-raw/innatedb.R
@@ -11,11 +11,7 @@ innatedb_init <- read_tsv(
 innatedb_filtered <- innatedb_init %>%
   filter(
     # We just want human interactions
-    ncbi_taxid_A == "taxid:9606(Human)" & ncbi_taxid_B == "taxid:9606(Human)",
-
-    # Only interested in protein-protein interactions
-    interactor_type_A == 'psi-mi:"MI:0326"(protein)' &
-      interactor_type_B == 'psi-mi:"MI:0326"(protein)'
+    ncbi_taxid_A == "taxid:9606(Human)" & ncbi_taxid_B == "taxid:9606(Human)"
   )
 
 innatedb_trimmed <- innatedb_filtered %>%
@@ -23,7 +19,8 @@ innatedb_trimmed <- innatedb_filtered %>%
     "ensembl_gene_A" = alt_identifier_A,
     "ensembl_gene_B" = alt_identifier_B
   ) %>%
-  mutate(across(everything(), str_remove, pattern = "ensembl\\:"))
+  mutate(across(everything(), str_remove, pattern = "ensembl\\:")) %>%
+  distinct(ensembl_gene_A, ensembl_gene_B)
 
 
 # Get gene mapping from biomaRt
@@ -33,11 +30,22 @@ biomart_mapping <- biomaRt::getBM(
   verbose    = TRUE
 )
 
-innatedb <- innatedb_trimmed %>%
+innatedb_mapped <- innatedb_trimmed %>%
   left_join(biomart_mapping, by = c("ensembl_gene_A" = "ensembl_gene_id")) %>%
   rename("hgnc_symbol_A" = hgnc_symbol) %>%
   left_join(biomart_mapping, by = c("ensembl_gene_B" = "ensembl_gene_id")) %>%
   rename("hgnc_symbol_B" = hgnc_symbol) %>%
   relocate(ends_with("A"))
+
+innatedb <- innatedb_mapped %>%
+  group_by(ensembl_gene_A) %>%
+  filter(n() < 1000) %>%
+  ungroup() %>%
+  group_by(ensembl_gene_B) %>%
+  filter(n() < 1000) %>%
+  ungroup()
+
+innatedb %>% count(hgnc_symbol_A) %>% arrange(desc(n))
+innatedb %>% count(hgnc_symbol_B) %>% arrange(desc(n))
 
 usethis::use_data(innatedb, overwrite = TRUE)
