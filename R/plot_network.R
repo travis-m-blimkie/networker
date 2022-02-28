f857f22c27ea6e1f70ebf844466d75f4ffd40f8c
Travis <travis.m.blimkie@gmail.com>
2022-02-28 10:26:05 -0800

    Added another argument for segment length. Increased default label size.

--- R/plot_network.R
+++ R/plot_network.R
@@ -22,6 +22,10 @@
 #' @param label_colour Colour of node labels, defaults to "black"
 #' @param label_face Font face for node labels, defaults to "bold"
 #' @param label_padding Padding around the label, defaults to 0.25 lines.
+#' @param min_seg_length Minimum length of lines to be drawn from labels to
+#'   points. The default specified here is 0.25, half of the normal default
+#'   value.
+#'
 #' @param seed Number used in call to `set.seed()` to allow for reproducible
 #'   network generation
 #'
@@ -44,19 +48,20 @@
 plot_network <- function(
   network,
   fill_column,
-  layout        = "kk",
-  edge_colour   = "grey50",
-  edge_alpha    = 0.2,
-  node_size     = c(3, 9),
-  int_colour    = "grey70",
-  label         = FALSE,
+  layout         = "kk",
+  edge_colour    = "grey50",
+  edge_alpha     = 0.2,
+  node_size      = c(3, 9),
+  int_colour     = "grey70",
+  label          = FALSE,
   label_column,
-  label_filter  = 0,
-  label_size    = 4,
-  label_colour  = "black",
-  label_face    = "bold",
-  label_padding = 0.25,
-  seed          = 1
+  label_filter   = 0,
+  label_size     = 5,
+  label_colour   = "black",
+  label_face     = "bold",
+  label_padding  = 0.25,
+  min_seg_length = 0.25,
+  seed           = 1
 ) {
 
   if (layout == "force_atlas") {
@@ -98,7 +103,8 @@ plot_network <- function(
         colour        = label_colour,
         fontface      = label_face,
         check_overlap = TRUE,
-        box.padding   = label_padding
+        box.padding   = label_padding,
+        min.segment.length = min_seg_length
       ) +
       scale_size_continuous(range = node_size, guide = "none") +
       theme(plot.margin = unit(rep(0.05, 4), "cm"))
