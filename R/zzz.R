39ce061473ec9e1c903efce80eaab782007a6d2b
Travis <travis.m.blimkie@gmail.com>
2022-02-28 10:29:39 -0800

    Link in startup message prints on a new/separate line.

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
 .onAttach <- function(...) {
   packageStartupMessage(
+    appendLF = TRUE,
     stringr::str_wrap(
       paste0(
         "Thanks for using networker v", utils::packageVersion("networker"),
         "! If you encounter any bugs or problems, please submit an issue at ",
-        "the Github page: ",
-        "https://github.com/travis-m-blimkie/networker/issues"
+        "the Github page:"
       ),
       width = getOption("width")
-    )
+    ),
+    "\nhttps://github.com/travis-m-blimkie/networker/issues"
   )
 }
