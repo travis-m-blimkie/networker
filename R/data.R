2af64ef409f281e3015387500ff4308ce178122f
Travis <travis.m.blimkie@gmail.com>
2022-03-01 17:38:51 -0800

    Cleaning up and straightening out the data being used to build the interaction networks.

--- R/data.R
+++ R/data.R
@@ -1,10 +1,13 @@
-#' Human experimentally verified PPI data downloaded from InnateDB
+#' Experimentally verified human PPI data downloaded from InnateDB
 #'
 #' A data frame containing human PPI data from InnateDB, from the entry
 #' "All Experimentally Validated Interactions (updated weekly)" at
-#' <https://innatedb.com/redirect.do?go=downloadImported>
+#' <https://innatedb.com/redirect.do?go=downloadImported>. A few important steps
+#' have been taken to clean up the data, namely the removal of duplicate
+#' interactions, and removing interactions that have the same components but are
+#' swapped between A and B
 #'
-#' @format A data frame with 323626 rows and 4 columns:
+#' @format A data frame with 152259 rows and 4 columns:
 #' \describe{
 #'   \item{ensembl_gene_A}{Ensembl gene ID for the first gene/protein in the
 #'     interaction}
@@ -16,23 +19,3 @@
 #'     interaction}
 #' }
 "innatedb_exp"
-
-
-#' Human PPI data downloaded from InnateDB
-#'
-#' A data frame containing human PPI data from InnateDB, from the entry
-#' "InnateDB Curated Protein-Protein Interactions (updated weekly)" at
-#' <http://innatedb.com/redirect.do?go=downloadCurated>
-#'
-#' @format A data frame with 323626 rows and 4 columns:
-#' \describe{
-#'   \item{ensembl_gene_A}{Ensembl gene ID for the first gene/protein in the
-#'     interaction}
-#'   \item{hgnc_symbol_A}{HGNC symbol for the first gene/protein in the
-#'     interaction}
-#'   \item{ensembl_gene_B}{Ensembl gene ID for the second gene/protein in the
-#'     interaction}
-#'   \item{hgnc_symbol_B}{HGNC symbol for the second gene/protein in the
-#'     interaction}
-#' }
-"innatedb_ppi"
--- data-raw/innatedb_exp.R
+++ data-raw/innatedb_exp.R
@@ -1,3 +1,4 @@
+
 # Load packages
 library(tidyverse)
 
@@ -23,6 +24,11 @@ innatedb_trimmed <- innatedb_filtered %>%
   distinct(ensembl_gene_A, ensembl_gene_B)
 
 
+# Remove interactions that are the same, but reversed between the two columns
+innatedb_no_dups <-
+  innatedb_trimmed[!duplicated(data.frame(t(apply(innatedb_trimmed, 1, sort)))), ]
+
+
 # Get gene mapping from biomaRt
 biomart_mapping <- biomaRt::getBM(
   mart       = biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl"),
@@ -30,13 +36,15 @@ biomart_mapping <- biomaRt::getBM(
   verbose    = TRUE
 )
 
-innatedb_mapped <- innatedb_trimmed %>%
+innatedb_mapped <- innatedb_no_dups %>%
   left_join(biomart_mapping, by = c("ensembl_gene_A" = "ensembl_gene_id")) %>%
   rename("hgnc_symbol_A" = hgnc_symbol) %>%
   left_join(biomart_mapping, by = c("ensembl_gene_B" = "ensembl_gene_id")) %>%
   rename("hgnc_symbol_B" = hgnc_symbol) %>%
   relocate(ends_with("A"))
 
+
+# Remove proteins/genes with more than 1000 interactions (e.g. UBC)
 innatedb_exp <- innatedb_mapped %>%
   group_by(ensembl_gene_A) %>%
   filter(n() < 1000) %>%
@@ -45,7 +53,10 @@ innatedb_exp <- innatedb_mapped %>%
   filter(n() < 1000) %>%
   ungroup()
 
+
+# Get a quick summary of the top interactors
 innatedb_exp %>% count(hgnc_symbol_A) %>% arrange(desc(n))
 innatedb_exp %>% count(hgnc_symbol_B) %>% arrange(desc(n))
+
 
 usethis::use_data(innatedb_exp, overwrite = TRUE)
