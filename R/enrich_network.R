7fdae597387ac9bfe4d778b209c2d110209957ba
Travis <travis.m.blimkie@gmail.com>
2022-03-07 12:19:26 -0800

    Fixed enrich_network documentation, and modified external function usage.

--- R/enrich_network.R
+++ R/enrich_network.R
@@ -1,16 +1,21 @@
 #' Test a tidygraph network object for enriched Reactome pathways
 #'
-#' @param network tidygraph object, i.e. one output by `build_network`
+#' @param network tidygraph object, i.e. one output by `build_network` with
+#'   Ensembl IDs as node names
 #' @param filter Desired p-value threshold for filtering output pathways.
 #'   Defaults to 0.05.
 #' @param background Optional background genes used when testing
 #'
-#' @return
+#' @return A tibble of enriched Reactome pathways
+#'
 #' @export
 #'
-#' @import ReactomePA
 #' @import dplyr
 #'
+#' @references See <https://www.bioconductor.org/packages/ReactomePA/> for
+#'   details on ReactomePA's methods.
+#'
+#' @seealso <https://github.com/travis-m-blimkie/networker>
 #'
 enrich_network <- function(network, filter = 0.05, background = NULL) {
   node_table <- as_tibble(network)
@@ -22,13 +27,13 @@ enrich_network <- function(network, filter = 0.05, background = NULL) {
     OrgDb    = "org.Hs.eg.db"
   ) %>% pull(ENTREZID)
 
-  enrichPathway(
+  ReactomePA::enrichPathway(
     gene     = na.omit(input_entrez),
     organism = "human",
     universe = background
   ) %>%
-    pluck("result") %>%
-    remove_rownames() %>%
+    purrr::pluck("result") %>%
+    tibble::remove_rownames() %>%
     janitor::clean_names() %>%
     as_tibble() %>%
     filter(p_adjust < filter)
