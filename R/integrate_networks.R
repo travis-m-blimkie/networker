2c7364abf76af5d740b024b9aae8ecb9fe34c770
Travis <travis.m.blimkie@gmail.com>
2022-03-09 11:22:33 -0800

    Modified integrate_networks to handle 2+ inputs

--- R/integrate_networks.R
+++ R/integrate_networks.R
@@ -1,11 +1,10 @@
 #' Combine two (or more?) gene lists into a single integrated network
 #'
-#' @param df_a First data frame of input genes, with the first column containing
-#'   Ensembl gene IDs.
-#' @param df_b Second data frame. The name and contents of its first column
-#'   should be the same as `df_a`
-#' @param col Column containing Ensembl gene IDs; must be present in both `df_a`
-#'   and `df_b`
+#' @param df_list List of data frames; data to be integrated into a single
+#'   network. If the list items do not have names, they will be assigned names
+#'   using `LETTERS`.
+#' @param col Column containing Ensembl gene IDs; must be present in all
+#'   elements of `df_list`.
 #' @param order Desired type of output network ("zero", "first", "min_simple" or
 #'   "min_steiner"); passed to `build_network()`
 #' @param ppi_data Data frame of PPI data; must contain rows of interactions as
@@ -28,17 +27,19 @@
 #'
 #' @seealso <https://www.github.com/travis-m-blimkie/networker>
 #'
-integrate_networks <- function(df_a, df_b, col, order, ppi_data = innatedb_exp, seed = 1) {
+integrate_networks <- function(df_list, col, order, ppi_data = innatedb_exp, seed = 1) {
 
-  # Clean the input df_a and df_b
-  input_dfs <- list("A" = df_a, "B" = df_b)
+  # Clean the input df_list, starting with giving them names if not present
+  if (is.null(names(df_list))) {
+    names(df_list) <- LETTERS[seq(1, length(df_list))]
+  }
 
-  input_dfs_clean <- input_dfs %>% purrr::imap(
+  input_dfs_clean <- df_list %>% purrr::imap(
     ~dplyr::select(.x, "name" = col) %>%
       mutate(source = .y)
   )
 
-  # Build a network for df_a and df_b
+  # Build a network for each data frame in df_list
   message("==> Build individual networks...")
   individual_networks <- input_dfs_clean %>% purrr::map(
     ~build_network(
@@ -50,11 +51,11 @@ integrate_networks <- function(df_a, df_b, col, order, ppi_data = innatedb_exp, 
     )
   )
 
-  # Build the combined/integrated network
+  # Build the combined/integrated network by first joining the inputs
   message("\n==> Build combined network...")
-  combined_seed_nodes <-
-    full_join(input_dfs_clean[[1]], input_dfs_clean[[2]], by = "name") %>%
-    tidyr::unite(contains("source"), col = "source_network", na.rm = TRUE)
+  combined_seed_nodes <- bind_rows(input_dfs_clean) %>%
+    group_by(name) %>%
+    summarise(source_network = paste(source, collapse = "_"))
 
   combined_network <- build_network(
     df       = combined_seed_nodes,
