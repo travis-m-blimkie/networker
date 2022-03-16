067b38558be7285fe0cafea06c61671bf21b24c6
Travis <travis.m.blimkie@gmail.com>
2022-03-16 15:34:59 -0700

    Added feature - if labels are turned on, the top 2% of nodes (based on hub score) have a different label colour to highlight the top hubs in the network.

--- R/plot_network.R
+++ R/plot_network.R
@@ -23,6 +23,9 @@
 #'   node labels, to prevent the network from being too crowded.
 #' @param label_size Size of node labels, defaults to 4.
 #' @param label_colour Colour of node labels, defaults to "black"
+#' @param hub_colour Colour of node labels for hubs. The top 2% of nodes (based
+#'   on calculated hub score) are highlighted with this colour, if `label =
+#'   TRUE`.
 #' @param label_face Font face for node labels, defaults to "bold"
 #' @param label_padding Padding around the label, defaults to 0.25 lines.
 #' @param min_seg_length Minimum length of lines to be drawn from labels to
@@ -62,6 +65,7 @@ plot_network <- function(
   label_filter   = 0,
   label_size     = 5,
   label_colour   = "black",
+  hub_colour     = "blue2",
   label_face     = "bold",
   label_padding  = 0.25,
   min_seg_length = 0.25,
@@ -108,28 +112,40 @@ plot_network <- function(
 
   if (label) {
 
+    hub_nodes <- as_tibble(network) %>%
+      arrange(desc(hub_score)) %>%
+      slice_head(n = ceiling(nrow(as_tibble(network)) * 0.02)) %>%
+      pull(name)
+
     network <- network %>%
-      mutate(node_label = case_when(
-        degree > label_filter ~ {{label_column}},
-        TRUE ~ NA_character_
-      ))
+      mutate(
+        node_label = case_when(
+          degree > label_filter ~ {{label_column}},
+          TRUE ~ NA_character_
+        ),
+        is_hub = case_when(
+          name %in% hub_nodes ~ "y",
+          TRUE ~ "n"
+        )
+      )
 
     ggraph(network, layout = layout_object) +
       geom_edge_link(show.legend = FALSE, alpha = edge_alpha, colour = edge_colour) +
       geom_node_point(aes(size = degree, fill = {{fill_column}}), pch = 21, colour = "grey70") +
       network_fill_geom +
       geom_node_text(
-        aes(label = node_label),
+        aes(label = node_label, colour = is_hub),
         size          = label_size,
         repel         = TRUE,
         family        = fontfamily,
-        colour        = label_colour,
         fontface      = label_face,
         check_overlap = TRUE,
+        show.legend   = FALSE,
         box.padding   = label_padding,
         min.segment.length = min_seg_length
       ) +
       scale_size_continuous(range = node_size, guide = "none") +
+      scale_colour_manual(values = c("y" = hub_colour, "n" = label_colour)) +
       labs(fill = stringr::str_wrap(
         janitor::make_clean_names(legend_name, "title"),
         7
