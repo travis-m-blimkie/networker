d55b4c70b7b1ace4d5e8967f6fef2bd86a23e147
Travis <travis.m.blimkie@gmail.com>
2022-03-24 14:55:37 -0700

    Changed default colours when doing an up/down colour mapping.

--- R/plot_network.R
+++ R/plot_network.R
@@ -114,7 +114,7 @@ plot_network <- function(
         )
 
       network_fill_geom <- scale_fill_manual(
-        values   = c("Up" = "firebrick", "Down" = "springgreen4"),
+        values   = c("Up" = "firebrick3", "Down" = "#23bd24ff"),
         na.value = int_colour
       )
 
