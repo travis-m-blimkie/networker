87b6d35316b661500d8057b8dc84b0adee9433a4
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-04-13 09:44:03 -0700

    Switched Entrez IDs to character, and improved the code generating the biomart_id_mapping_human object.

--- data-raw/biomart_id_mapping_human.R
+++ data-raw/biomart_id_mapping_human.R
@@ -1,22 +1,24 @@
 # Load the required packages
 library(biomaRt)
-library(dplyr)
+library(tidyverse)
 
 # Use `biomaRt::getBM()` to create the conversion table, with the three most
 # common human ID types.
-biomart_table_human_1 <- getBM(
+biomart_id_mapping_human_0 <- getBM(
   attributes = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id"),
   mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 )
 
-# Replace empty values with NA
-biomart_table_human_2 <- biomart_table_human_1 %>% replace(. == "", NA)
-
-# Keep only one row for each Ensembl gene
-biomart_id_mapping_human <- biomart_table_human_2 %>%
+# Tidy up the data
+biomart_id_mapping_human <- biomart_id_mapping_human_0 %>%
+  as_tibble() %>%
   rename("entrez_gene_id" = entrezgene_id) %>%
+  mutate(
+    across(everything(), as.character),
+    across(everything(), ~str_replace(.x, "^$", NA_character_))
+  ) %>%
   arrange(ensembl_gene_id, hgnc_symbol, entrez_gene_id) %>%
-  distinct(ensembl_gene_id, .keep_all = TRUE) %>%
-  as_tibble()
+  distinct()
 
+# Save data for use in the package
 usethis::use_data(biomart_id_mapping_human, overwrite = TRUE)
